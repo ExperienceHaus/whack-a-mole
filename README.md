08888d124f2ab87df8e2facde3f2f998ec101d96
Charlie Hawker <charlie.hawker@gmail.com>
2018-03-21 17:54:50 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Whack-a-mole
+
+This is a class project for Experience Haus FEWD 001 students.
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+
+// to process sass files
+gulp.task('styles', function() {
+  return gulp.src('./src/sass/**/*./scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+// to prepare javascript file(s)
+gulp.task('scripts', function() {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./js'));
+});
+
+// to optimise images
+gulp.task('images', function() {
+  return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./images'));
+});
+
+gulp.task('default', ['styles', 'scripts', 'images'], function() {
+  gulp.watch('./src/js/**/*.js', ['scripts']);
+  gulp.watch('./src/sass/**/*.scss', ['styles']);
+})
